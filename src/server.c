5bd9cf176ebcab227403b02e1dd23dec3d385187
Perry Kundert <perry@hardconsulting.com>
2015-10-19 06:32:46 -0600

    Correct variable name

--- src/server.c
+++ src/server.c
@@ -453,7 +453,7 @@ void Server_init()
     mqinit(mq_threads);
     if(zmq_ctx_get(ZMQ_CTX, ZMQ_MAX_SOCKETS) != mq_max_sockets) {
         if(zmq_ctx_set(ZMZMQ_CTX, ZMQ_MAX_SOCKETS, mq_max_sockets) < 0) {
-	    printf("Error setting 0mq ZMQ_MAX_SOCKETS to %d: %s.\n", max_sockets, strerror(errno));
+	    printf("Error setting 0mq ZMQ_MAX_SOCKETS to %d: %s.\n", mq_max_sockets, strerror(errno));
 	    exit(1);
 	}
     }
